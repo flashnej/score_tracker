bea78a364940a3a9669206dc99e6ec06b04f20cf
Jason Flashner <jasonflashner@Jasons-MacBook-Pro.local>
2021-04-05 16:22:53 -0400

    removed a debugger

--- app/controllers/api/v1/rounds_controller.rb
+++ app/controllers/api/v1/rounds_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::RoundsController < ApplicationController
         rounds = user.rounds
         output = []
         rounds.each do |round|
-            # binding.pry
             output.push({"id": round.id, "Course Name": round.course.name, "Score": round.score, "Course Slope": round.course.slope_rating, "Course Rating": round.course.course_rating})
         end
         render json: output
