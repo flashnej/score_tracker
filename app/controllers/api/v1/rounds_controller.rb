0d889e1871e2a545af7a0701a33cfb4391664b47
Jason Flashner <jasonflashner@Jasons-MacBook-Pro.local>
2021-04-05 16:21:51 -0400

    finished adding rounds

--- app/controllers/api/v1/rounds_controller.rb
+++ app/controllers/api/v1/rounds_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RoundsController < ApplicationController
+
     def index
         user = current_user
         rounds = user.rounds
@@ -8,5 +9,17 @@ class Api::V1::RoundsController < ApplicationController
             output.push({"id": round.id, "Course Name": round.course.name, "Score": round.score, "Course Slope": round.course.slope_rating, "Course Rating": round.course.course_rating})
         end
         render json: output
+    end
+
+    def create
+        user = current_user
+        course = Course.where({name: params["course"]})[0]
+        score = params["score"].to_i
+        round = Round.new(user: user, course: course, score: score)
+        if round.save! 
+            render json: {good: "Course added successfully"}
+        else
+            render json: {error: "Error adding round"}
+        end
     end
 end
